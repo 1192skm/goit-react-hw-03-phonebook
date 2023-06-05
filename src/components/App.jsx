e8d28ee8958d417731c8354b16a9f3121e6eb2a3
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-06-05 22:11:46 +0300

    Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import css from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
   addContact = ({ name, number }) => {
@@ -36,6 +31,18 @@ export class App extends Component {
   handleFilter = evt => {
     this.setState({ filter: evt.currentTarget.value });
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsContacts = JSON.parse(contacts);
+    if (parsContacts) {
+      this.setState({ contacts: parsContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const normFilter = this.state.filter.toLocaleLowerCase();
